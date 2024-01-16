effa200006fc1042d5f428e8677271ba47d386d2
ChileRoblox2 <156033618+ChileRoblox2@users.noreply.github.com>
2024-01-16 18:42:14 -0300

    Update ESP_Script.lua

--- ESP_Script.lua
+++ ESP_Script.lua
@@ -3,8 +3,8 @@ local Players = game:GetService("Players")
 local function highlightPlayer(player)
     local highlight = Instance.new("Highlight")
     highlight.Parent = player.Character
-    hightlight.FillColor = Color3.fromRGB(255, 0, 0)
-    hightlight.OutlineColor = Color3.fromRGB(0, 0, 255)
+    highlight.FillColor = Color3.fromRGB(255, 0, 0)
+    highlight.OutlineColor = Color3.fromRGB(0, 0, 255)
 end
 
 for _, player in ipairs(Players:GetPlayers()) do
